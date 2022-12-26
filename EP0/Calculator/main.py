a42852a2673bf715d88e76e988b9004fd6f17430
krystofhorak <krystof.horak@proton.me>
2022-12-26 16:10:35 +0100

    EP0: Adding a basic CLI calculator app

---
+++ EP0/Calculator/main.py
@@ -0,0 +1,11 @@
+print('Kalkulačka\n')
+
+prvni_cislo = int(input('Zadejte první číslo: '))
+druhe_cislo = int(input('Zadejte druhé číslo: '))
+
+print(f'\nSoučet: {prvni_cislo + druhe_cislo}')
+print(f'Rozdíl: {prvni_cislo - druhe_cislo}')
+print(f'Součin: {prvni_cislo * druhe_cislo}')
+print(f'Podíl:  {prvni_cislo / druhe_cislo}')
+
+print('\nDěkujeme za použití programu!')
